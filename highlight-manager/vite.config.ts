b719beb6fa92a0e6bfeb3c424427ea024181d067
wjkba <63542318+wjkba@users.noreply.github.com>
2024-11-03 12:53:29 +0100

    feat: pwa support

--- highlight-manager/vite.config.ts
+++ highlight-manager/vite.config.ts
@@ -1,9 +1,32 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Highlight Manager",
+        short_name: "Highlights",
+        description: "Manage your boox highlights",
+        icons: [
+          {
+            src: "web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

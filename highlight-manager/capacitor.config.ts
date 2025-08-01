45b18afb8fb26272fc55c650bb9e7c7bf0c83b87
wjkba <63542318+wjkba@users.noreply.github.com>
2025-08-01 10:22:34 +0200

    add capacitor

---
+++ highlight-manager/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wjkba.booxhighlights',
+  appName: 'boox-highlights',
+  webDir: 'dist'
+};
+
+export default config;
